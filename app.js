cb4b172d6397678d15c7dcc55b58c2972b20d873
juniordeveu <tomrys@juniordev.eu>
2023-02-18 17:16:08 +0100

    test ip client and cpu info

--- app.js
+++ app.js
@@ -4,19 +4,20 @@
 const http = require( 'http' )
 const os = require('os')
 const express = require( 'express' );
-const { cp } = require('fs');
+
 
 const app = express();
 
 app.get( '/test', ( req, res ) => {
- 
+    const dirName = (__dirname)
     const testIP = req.ip
+
     const cpuInfo = JSON.stringify( os.cpus() )
 
     res.writeHead(200, {'Content-Type':'text/plain', "Content-Type":"application/json"})
     
-    res.write( cpuInfo + "\n\n\nAdres IP \n"+ testIP)
-
+    res.write( cpuInfo + "\n\n\nAdres IP \n"+ testIP + "\n\n\nDir name" + dirName)
+    
     res.end()
 } )
 
