cfafec940a13f87b0fca1f3f03bc72024e6998cf
juniordeveu <tomrys@juniordev.eu>
2023-02-18 17:20:32 +0100

    test ip client

--- app.js
+++ app.js
@@ -11,12 +11,19 @@ const app = express();
 app.get( '/test', ( req, res ) => {
     const dirName = (__dirname)
     const testIP = req.ip
+    const testIPs = req.ips
 
     const cpuInfo = JSON.stringify( os.cpus() )
 
     res.writeHead(200, {'Content-Type':'text/plain', "Content-Type":"application/json"})
     
-    res.write( cpuInfo + "\n\n\nAdres IP \n"+ testIP + "\n\n\nDir name" + dirName)
+    res.write( cpuInfo + " \n\n\nAdres IP \n "
+                        + testIP    
+                        + " \n\n\nDir name "  
+                        + dirName 
+                        + " \n\n\n IPS \n " 
+                        + testIPs 
+                        );
     
     res.end()
 } )
